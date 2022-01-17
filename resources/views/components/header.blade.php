a3e1a944952dcad36d1eb26733f82100edc0a9b7
Mithun Sutradhar <ms@pop-os.localdomain>
2022-01-17 17:03:44 +0600

    header part complete

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,19 @@
+<div class="fixed w-full p-4 px-12 flex  justify-between items-center z-30">
+<div class="min-w-max">
+    <img width="100" src="/img/logo.png" alt="">
+
+</div>
+<div class="w-full">
+    <ul class="flex justify-center text-white">
+        <li><a class="inline-block p-4" href="">Land</a></li>
+        <li><a class="inline-block p-4" href="">Villa</a></li>
+        <li><a class="inline-block p-4" href="">Appartment</a></li>
+        <li><a class="inline-block p-4" href="">About Us</a></li>
+        <li><a class="inline-block p-4" href="">Contact Us</a></li>
+    </ul>
+</div>
+<div class="min-w-max text-3xl">
+    <a href="">🇺🇸</a>
+    <a href="">🇹🇷</a>
+</div>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<x-guest-layout>
+<div class="relative z-10 pt-52 pb-64 bg-cover bg-center" style="background-image:url('/img/hero.jpg')">
+<div class="absolute h-full w-full bg-black opacity-70 top-0 left-0 z-10"></div>
+<div class="container relative z-20 text-white text-center text-2xl">
+<h2 class="font-bold text-5xl mb-5">Lorem ipsum dolor,<br> sit amet consectetur adipisicing elit. Non, laboriosam.</h2>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, quibusdam.</p>
+</div>
+</div>
+</x-guest-layout>
