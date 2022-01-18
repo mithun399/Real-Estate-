c131251be2e5eceaf0b787a853528c1e7244bc36
Mithun Sutradhar <ms@pop-os.localdomain>
2022-01-18 08:37:13 +0600

    Home Page added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,17 @@
 require('./bootstrap');
 
 import Alpine from 'alpinejs';
+import jQuery from './jquery.slim.min';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+jQuery(window).scroll(function() {
+    const scroll = jQuery(window).scrollTop();
+    if (scroll >= 50) {
+        jQuery('.sticky-header').addClass('sticky-header-active');
+    } else {
+        jQuery('.sticky-header').removeClass('sticky-header-active');
+    }
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,16 +6,164 @@
 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, quibusdam.</p>
 </div>
 </div>
-<div class="py-32 text-center">
+<!--start searching form-->
+<div class="rounded-lg container bg-white p-4 relative -m-10 z-30 shadow-lg">
+<form action="" method="GET" class="flex justify-between search-form">
+    <div class="flex flex-1 justify-between">
+        <div class="single-search-field">
+          
+            <select name="sale" id="sale" class="border-0">
+                <option value="">Rent/Sale</option>
+                <option  value="1">Rent</option>
+                <option  value="2">Sale</option>
+            </select>
+        </div>
+        <div class="search-field-border"></div>
+        <div class="single-search-field">      
+            <select name="location" id="sale" class="border-0">
+                <option value="">Location</option>
+                <option class="noTranslate">Location</option>
+                
+            </select>
+        </div>
+
+        <div class="search-field-border"></div>
+        <div class="single-search-field">
+            
+            
+            
+            <select name="type" id="type" class="border-0">
+                <option value="">Type</option>
+                <option value="land">Land</option>
+                <option value="Apartment">Type</option>
+                <option  value="villa">Villa</option>
+            </select>
+        </div>
+        <div class="search-field-border"></div>
+        <div class="single-search-field">
+            
+            
+            
+            <select name="price" id="price" class="border-0">
+                <option value="">Price</option>
+                <option  value="100000">1000</option>
+                <option   value="200000">2000</option>
+                <option  value="300000">3000</option>
+                <option   value="400000">4000</option>
+                <option value="500000">5000</option>
+                <option value="500001">6000</option>
+            </select>
+        </div>
+        <div class="search-field-border"></div>
+        <div class="single-search-field">
+            
+            
+            
+            <select name="bedrooms" id="bedrooms" class="border-0">
+                <option value="">Bedrooms</option>
+                
+            </select>
+        </div>
+    </div>
+    <div class="flex justify-between items-end flex-1 ml-5 search-submit">
+        <input value="" type="search" name="search" placeholder="Enter Text..."
+            class="rounded-lg px-4 py-2 w-full mr-4 border-2 text-sm">
+        <button type="submit" class="btn">Search</button>
+    </div>
+</form>
+    </div>
+<!--End Searching form-->
+<div class="py-10 text-center">
     <div class="container">
         <h2 class="font-bold text-4xl mb-6">Lorem ipsum dolor sit, amet consectetur <br> adipisicing elit. Doloribus <span class="underline">excepturi vitae</span> Lorem, ipsum. </h2>
         <p class="text-gray-500 text-2xl font-bold mb-7">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, adipisci.</p>
 
         <a class="border-2 border-gray-700 rounded-2xl text-xl px-2 py-3 inline-block" href="">Start The Searching</a>
-        <h2 class="font-bold text-4xl mb-6 mt-20">Lorem ipsum dolor sit, amet consectetur <br> adipisicing elit.<span class="underline">excepturi vitae</span></h2>
     </div>
 
 </div>
 
+<div class="container text-center">
+        <h2 class="section-title font-bold text-4xl mb-6 mt-20">Why should you choose a property <br>with the help of our service?</h2>
+        <div class="flex -mx-3 flex-wrap justify-between mt-10">
+            <div class="w-3/12  mt-10 mx-3 text-base">
+                <h3 class="mb-2">Huge selection of real estate</h3>
+                <p class="text-xs text-center mx-auto w-4/6">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Dolore</p>
+            </div>
+            <div class="w-3/12  mt-10 mx-3 text-base">
+                <h3 class="mb-2">Huge selection of real estate</h3>
+                <p class="text-xs text-center mx-auto w-4/6">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Dolore</p>
+            </div>
+            <div class="w-3/12  mt-10 mx-3 text-base">
+                <h3 class="mb-2">Huge selection of real estate</h3>
+                <p class="text-xs text-center mx-auto w-4/6">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Dolore</p>
+            </div>
+            <div class="w-3/12  mt-10 mx-3 text-base">
+                <h3 class="mb-2">Huge selection of real estate</h3>
+                <p class="text-xs text-center mx-auto w-4/6">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Dolore</p>
+            </div>
+            <div class="w-3/12  mt-10 mx-3 text-base">
+                <h3 class="mb-2">Huge selection of real estate</h3>
+                <p class="text-xs text-center mx-auto w-4/6">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Dolore</p>
+            </div>
+            <div class="w-3/12  mt-10 mx-3 text-base">
+                <h3 class="mb-2">Huge selection of real estate</h3>
+                <p class="text-xs text-center mx-auto w-4/6">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Dolore</p>
+            </div>
+        </div>
+    </div>
+    <div class="container text-center pt-14">
+        <h2 class="section-title">More information about us</h2>
+        <div class="relative mt-10 mb-14 bg-cover rounded-xl py-24 bg-center"
+            style="background-image: url('/img/hero-bg.jpg')">
+            <div class="absolute w-full h-full rounded-xl opacity-50 bg-black left-0 top-0"></div>
+            <div class="relative z-20">
+                <a href="" class="text-white text-xl flex flex-col justify-center items-center"><span
+                        class="border-2 border-white w-12 h-12 text-center pt-1 pl-1 leading-10 text-2xl hover:border-yellow-500 duration-200 rounded-full mb-2"><i
+                            class="fa fa-play"></i></span>Watch the video</a>
+            </div>
+        </div>
+        <div class="text-xl">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam architecto autem corporis debitis
+                dignissimos doloremque eius error explicabo impedit iste libero magnam, natus officiis omnis quasi
+                ratione sequi totam, vero.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam architecto
+                autem corporis debitis
+                dignissimos doloremque eius error explicabo impedit iste libero magnam, natus officiis omnis quasi
+                ratione sequi totam, vero.</p>
 
+        </div>
+    </div>
+    <div class="container pt-14">
+        <div class="flex justify-between ">
+            <div class="flex-1 mr-10 text-lg leading-normal">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores deleniti ipsam iure laborum nemo
+                    non odit quis quod, sint tempore. Ab culpa cupiditate error, eum incidunt officiis rem sapiente
+                    voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores deleniti ipsam iure
+                    laborum nemo non odit quis quod, sint tempore. Ab culpa cupiditate error, eum incidunt officiis rem
+                    sapiente voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores deleniti
+                    ipsam iure laborum nemo non odit quis quod, sint tempore. Ab culpa cupiditate error, eum incidunt
+                    officiis rem sapiente voluptate.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+                    deleniti ipsam iure laborum nemo non odit quis quod, sint tempore. Ab culpa cupiditate error, eum
+                    incidunt officiis rem sapiente voluptate</p>
+            </div>
+            <div class="flex-1 ml-10">
+                <img src="/img/hero.jpg" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="container pt-14">
+        <div class="flex justify-center items-center">
+            <a href="" class="border-2 border-gray-700 rounded-2xl text-xl px-2 py-3 inline-block">Start searching with filters</a>
+            <p class="mx-10">or</p>
+            <a href="" class="border-2 border-gray-700 rounded-2xl text-xl px-2 py-3 inline-block">Start searching on the map</a>
+        </div>
+    </div>
+
+    
 </x-guest-layout>
